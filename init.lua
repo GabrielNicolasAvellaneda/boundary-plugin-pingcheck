94d683871051a21d7c385c61ed0e9e28b91036c7
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-04-23 00:08:11 -0300

    Added support for multiple hosts

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
 local framework = require('./modules/framework')
 local CommandOutputDataSource = framework.CommandOutputDataSource
+local PollerCollection = framework.PollerCollection
+local DataSourcePoller = framework.DataSourcePoller
 local Plugin = framework.Plugin
 local los = require('los')
 local table = require('table')
 local string = require('string')
 
 local isEmpty = framework.string.isEmpty
+local clone = framework.table.clone
+
+local params = framework.params 
+params.name = 'Boundary Pingcheck plugin'
+params.version = '1.1'
 
 local commands = {
   linux = { path = '/bin/ping', args = {'-n', '-w 2', '-c 1'} },
@@ -19,7 +26,22 @@ if ping_command == nil then
   process:exit(-1)
 end
 
-table.insert(ping_command.args, 'www.google.com')
+local function createPollers (params, ping_command) 
+  local pollers = PollerCollection:new() 
+  for i, item in ipairs(params.items) do
+    
+    local cmd = clone(ping_command)
+    table.insert(cmd.args, item.host)
+    cmd.info = item.source
+
+    local data_source = CommandOutputDataSource:new(cmd)
+    local poll_interval = tonumber(item.pollInterval or params.pollInterval) * 1000
+    local poller = DataSourcePoller:new(poll_interval, data_source)
+    pollers:add(poller)
+  end
+
+  return pollers
+end
 
 local function parseOutput(context, output) 
   
@@ -52,18 +74,14 @@ local function parseOutput(context, output)
   return -1
 end
 
-local data_source = CommandOutputDataSource:new(ping_command)
-
-local params = { pollInterval = 2000 }
-params.name = 'Boundary Pingcheck plugin'
-params.version = '1.1'
-local plugin = Plugin:new(params, data_source)
+local pollers = createPollers(params, ping_command)
+local plugin = Plugin:new(params, pollers)
 
 function plugin:onParseValues(data) 
   local result = {}
 
-  result['PING_RESPONSETIME'] = parseOutput(self, data)
-
+  local value = parseOutput(self, data['output'])
+  result['PING_RESPONSETIME'] = { value = value, source = data['info'] }
   return result
 end
 
