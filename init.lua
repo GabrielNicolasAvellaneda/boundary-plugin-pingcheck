db07617833b06686814b3ec5e661037154f2997b
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-04-22 23:09:39 -0300

    Initial version using the framework, hardcoded parameters and only accept one host

--- init.lua
+++ init.lua
@@ -1,101 +1,70 @@
-local boundary = require('boundary')
-local ping = require('ping')
-local timer = require('timer')
-local async = require('async')
+local framework = require('./modules/framework')
+local CommandOutputDataSource = framework.CommandOutputDataSource
+local Plugin = framework.Plugin
+local los = require('los')
+local table = require('table')
+local string = require('string')
 
-local HOST_IS_DOWN = -1
+local isEmpty = framework.string.isEmpty
 
-local param = boundary.param
+local commands = {
+  linux = { path = '/bin/ping', args = {'-n', '-w 2', '-c 1'} },
+  win32 = { path = 'C:/windows/system32/ping.exe', args = {'-n', '1', '-w', '3000'} },
+  darwin = { path = '/sbin/ping', args = {'-n', '-t 2', '-c 1'} }
+}
 
---[[ 
-If we do not have a poll Interval for the plugin set it
-each host has its own poll interval
-]]
-
-local pollInterval = param.pollInterval or 1000
-
--- Keep track of the host's last polled time so we don't hammer the host
-local previous = {}
-
-function logSuccess(source, duration)
-    io.stdout:write('PING_RESPONSETIME '..duration..' '..source..'\n')
+local ping_command = commands[los.type()] 
+if ping_command == nil then
+  io.stderr:write('Your platform is not supported.  We currently support Linux, Windows and OSX\n')
+  process:exit(-1)
 end
 
-function logFailure(err, source, duration)
-    if (err) then
-        io.stderr:write(err .. '\n')
-    else
-        io.stdout:write('PING_RESPONSETIME '..HOST_IS_DOWN..' '..source..'\n')
+table.insert(ping_command.args, 'www.google.com')
+
+local function parseOutput(context, output) 
+  
+  assert(output ~= nil, 'parseOutput expect some data')
+
+  if isEmpty(output) then
+    context:emit('error', 'Unable to obtain any output.')
+    return
+  end
+
+  if (string.find(output, "unknown host") or string.find(output, "could not find host.")) then
+    context:emit('error', 'The host ' .. context.args[#context.args] .. ' was not found.')
+    return
+  end
+
+  local index = 0
+  local prevIndex = 0
+  while true do
+    index = string.find(output, '\n', prevIndex+1) 
+    if index == nil then break end
+
+    local line = string.sub(output, prevIndex, index-1)
+    local _, _, time  = string.find(line, "time=([0-9]*%.?[0-9]+)")
+    if(time) then 
+      return tonumber(time)
     end
+    prevIndex = index
+  end
+
+  return -1
 end
 
-function complete(err)
-    if (err) then io.stderr:write(err) end
+local data_source = CommandOutputDataSource:new(ping_command)
+
+local params = { pollInterval = 2000 }
+params.name = 'Boundary Pingcheck plugin'
+params.version = '1.1'
+local plugin = Plugin:new(params, data_source)
+
+function plugin:onParseValues(data) 
+  local result = {}
+
+  result['PING_RESPONSETIME'] = parseOutput(self, data)
+
+  return result
 end
 
--- Validate we have Hosts to ping
-if (not param.items) then
-    io.stdout:write('No configuration has been setup yet, so we\'re exiting\n')
-    os.exit(1);
-end
-
--- Validate the hosts intervals
-table.foreach(param.items, function(hIndex)
-
-    local host = param.items[hIndex]
-    local pollInterval = 5
-
-    if host.pollInterval then
-        pollInterval = tonumber(host.pollInterval)
-    end
-
-    --Lets assume that the poll interval associated with hosts is provided in seconds
-
-    host.pollInterval = pollInterval
-
-end)
-
-function pingHost(host, cb)
-
-    -- Check if we need to poll again
-
-    local source = host.source
-
-    if(not previous[source]) then
-        previous[source] = os.time()
-    end    	
-
-    local now = os.time()
-
-    if ( (previous[source] + host.pollInterval) > now ) then
-        return cb(nil)
-    else
-        previous[source] = now
-    end
-
-    -- Ping the host
-    ping.ping(host.host, function(err, responseTime)
-    
-        if (err) then
-            logFailure(err, source, responseTime)
-
-        elseif (responseTime == nil or responseTime < 0) then
-            logFailure(nil, source, responseTime)
-
-        else
-            logSuccess(source, responseTime)
-        end        
-
-        return cb(nil)
-    end)
-
-end
-
-function poll()
-
-    async.forEach(param.items, pingHost, complete)
-    timer.setTimeout(pollInterval, poll)
-end
-
--- Lets get the party started
-poll()
+plugin:run()
